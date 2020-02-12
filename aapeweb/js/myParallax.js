97088682fe60bff3482d5d2bb9d8ae194505830b
Ali <brucwayn53@gmail.com>
2020-02-11 17:51:59 -0800

    revision by ali 1

--- aapeweb/js/myParallax.js
+++
@@ -1,8 +0,0 @@
-// var image = document.getElementsByClassName('collageDesktop');
-// new simpleParallax(image);
-
-var images = document.querySelectorAll('img.naggers');
-new simpleParallax(images, {
-    orientation: 'up',
-    scale: 1.1,
-});
